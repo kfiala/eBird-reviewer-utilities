6e5ff9a93cfdb267b4cf64314caf56674cb3eae4
Kent Fiala <Kent.Fiala@gmail.com>
2022-12-30 13:29:57 -0500

    Support for submission window

--- utilities.js
+++ utilities.js
@@ -11,27 +11,30 @@ if (window.location.href.includes('https://review.ebird.org/admin/review')) {
 
 	let lookup = {};
 	var deferToggle;
+	const parser = new DOMParser();
 	//
 	// Establish whether we are in review mode or exotics mode, or neither.
 	// Review mode includes both regular review (review.htm) and history review (reviewObs.htm)
 	// Both review queue and search results can be either review or exotics.
 	//
-	let reviewMode;
-	if (document.getElementById('bulkactions')) {
-		reviewMode = document.getElementById('bulkactions').getAttribute('class'); //'exotic' or 'actionbar-fixed'
-	} else {
-		reviewMode = 'no records';
+	let reviewMode = '';
+	let reviewForm = document.getElementById('reviewForm');
+	if (reviewForm) {
+		let span = document.getElementsByClassName('reviewmode-current')[0];
+		if (span) {
+			reviewMode = span.textContent;	// "Review mode" or "Exotics mode"
+		} 
 	}
 
 	//
 	// Adjust the widths of "Review decision", "Reason", and "Notes" inputs
 	//
 	switch (reviewMode) {
-		case 'exotic':
+		case 'Exotics mode':
 			document.getElementById('newExoticCategory').style='width:125px';
 			document.getElementById('notes').style='width:300px';
 			break;
-		case 'actionbar-fixed':
+		case 'Review mode':
 			document.getElementById('resultingValid').style='width:160px';
 			document.getElementById('reasonCode').style='width:300px';
 			document.getElementById('notes').style='width:300px';
@@ -57,14 +60,14 @@ if (window.location.href.includes('https://review.ebird.org/admin/review')) {
 
 	//
 	// Check if we have listnav, and if so get its webring element.
-	// This will be true for regular review and exotic review but not for history review
+	// This will be true for regular review (normal or exotic) but not for history or submission review
 	//
 	var Fwebring='';
 	if (document.getElementById("listnav")) {
 		Fwebring = document.getElementById("listnav").querySelector('.webring');
 	}
 
-	if (Fwebring) {	// If we are in regular review or exotic review
+	if (Fwebring) {	// If we are in regular review 
 		var mainTable = document.getElementById('contents');
 		if (mainTable) {	// If we have a table of records, e.g., not "Congratulations! You have no more records to review"
 		//	First set up the CSV download
@@ -78,7 +81,6 @@ if (window.location.href.includes('https://review.ebird.org/admin/review')) {
 				var row = [];
 				var subid, species, evidence, count, obsdate, user, locname, county, state, validity, status, dayOfYear;
 				var checklist, chklstCell, chklstLink;
-				const parser = new DOMParser();
 				let OBS;
 
 				elTr.querySelectorAll('td').forEach(function(Cell) {
@@ -287,14 +289,13 @@ if (window.location.href.includes('https://review.ebird.org/admin/review')) {
 			createOopsControl();
 			document.getElementById('listnav').insertBefore(createRecallText(),null);
 		}
-	} else if (document.querySelector('.obsreview_species')) {	// History screen
-		const dparser = new DOMParser();
+	} else if (location.pathname == '/admin/reviewObs.htm') {	// History window
 	//	From, e.g.,
 	//<h3 class="obsreview_species"><a href="https://birdsoftheworld.org/bow/species/snogoo/cur/introduction">Snow Goose</a>
 	//<h3 class="obsreview_species"><a href="https://ebird.org/species/passer1">passerine sp.</a>
 	//	parse out snogoo or passer1
 		const anchor = document.querySelector('.obsreview_species').innerHTML;
-		const urlPieces = dparser.parseFromString(anchor,"text/html").body.firstChild.href.split('/');
+		const urlPieces = parser.parseFromString(anchor,"text/html").body.firstChild.href.split('/');
 		let sixcode='';
 		for (let up=0; up < urlPieces.length; up++) {
 			if (urlPieces[up] === 'species') {
@@ -302,7 +303,7 @@ if (window.location.href.includes('https://review.ebird.org/admin/review')) {
 				break;
 			}
 		}
-		let species = dparser.parseFromString(anchor,"text/html").body.firstChild.textContent;
+		let species = parser.parseFromString(anchor,"text/html").body.firstChild.textContent;
 		const urlParams = new URLSearchParams(location.search);
 		let OBS = '';
 		if (urlParams.has('obsID')) {
@@ -314,7 +315,7 @@ if (window.location.href.includes('https://review.ebird.org/admin/review')) {
 	//	<td class="name"><a target="_blank" href="https://www.google.com/maps/place/35.727862,-78.776077">
 	//	parse out 35.727862,-78.776077
 		const tr = document.querySelector('.obsreview_table').querySelectorAll('td')[1].innerHTML;
-		const coords = dparser.parseFromString(tr,'text/html').body.firstChild.href.split('/')[5].split(',');
+		const coords = parser.parseFromString(tr,'text/html').body.firstChild.href.split('/')[5].split(',');
 		const Y = parseFloat(coords[0]);
 		const X = parseFloat(coords[1]);
 	//	Set up the URL for the species map
@@ -324,6 +325,34 @@ if (window.location.href.includes('https://review.ebird.org/admin/review')) {
 			'&env.maxX=' + (X+0.5) +
 			'&env.maxY=' + (Y+0.5) ;
 		finishMapURL(URL, OBS);
+	} else if (location.pathname == '/admin/reviewSub.htm') {	// Submission window
+		let theForm = document.getElementById('reviewForm');
+		let theTable = theForm.getElementsByTagName('tbody')[0];
+		let Class, html, el;
+		if (theTable) {
+			theTable.querySelectorAll('tr').forEach(function (elTr) {
+				// Extract the data from each row 
+				let OBS;
+				elTr.querySelectorAll('td').forEach(function (Cell) {
+					// Look at each column cell in this row of the table
+					Class = Cell.getAttribute('class').split(' ')[0];
+					html = parser.parseFromString(Cell.innerHTML, "text/html");
+					el = html.body.firstChild;
+
+					switch (Class) {
+						case "select":
+							OBS = el.getAttribute('value');
+							break;
+						case "species":
+							if (el.nodeName === 'LABEL') {
+								lookup[OBS] = el.textContent.trim();	// species name
+							}
+							break;
+						default:
+					}
+				});
+			});
+		}
 	}
 }
 
