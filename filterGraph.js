98a6f68e40665ab5d1635519d9d3e858a47f7d66
kfiala <Kent.Fiala@gmail.com>
2024-01-21 17:19:44 -0500

    Null click handler for filter graph button

--- filterGraph.js
+++ filterGraph.js
@@ -14,19 +14,23 @@ if (document.getElementById("region-name-codes"))
 		greenButton.setAttribute("name",sixcode);
 		greenButton.setAttribute("title",species + " bar chart");
 		greenButton.style.height = "14px";
+		greenButton.style.width = "22px";
 		greenButton.style.backgroundColor = "green";
 		greenButton.style.marginTop = "12px";
 
 		tr = document.getElementById(sixcode);
 		td = tr.insertCell(-1);
-		td.setAttribute("width","12px");
+		td.setAttribute("width","22px");
 		td.appendChild(greenButton);
 
 		greenButton.addEventListener('mouseup', function (event) {
+			// Has to be mouseup rather than click to prevent "enter" in the filter from having the same effect
 			let thisYear = new Date().getFullYear();
 			window.open("https://ebird.org/barchart?r="+regionCodes+"&bmo=1&emo=12&byr=1900&eyr=" + thisYear + "&spp="+event.target.name);
 			event.preventDefault();
 			return false;
 		});
+		// mouseup is always accompanied by click. Need a null click handler to prevent page reload caused by click.
+		greenButton.addEventListener('click', (event) => { event.preventDefault() });
 	}
 }
